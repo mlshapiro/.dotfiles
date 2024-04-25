4062f9e4c77cd7282b6359b518cb56a8b0227aa0
Marc Shapiro <git@ms.tengr.net>
2024-04-25 12:56:23 -0400

    Remove sublime packages, add iterm2 preferences

--- os/macos/setup.sh
+++ os/macos/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# remove terminal from dark mode theme
+defaults write com.apple.Terminal NSRequiresAquaSystemAppearance -bool YES
+
+# show all hidden files
+defaults write com.apple.finder AppleShowAllFiles -boolean true; killall Finder
+
+# install services from homebrew
 brew install wget
 brew install gnupg
 brew install git
@@ -18,11 +25,21 @@ brew install miniforge
 pipx install bpytop
 pipx install pipenv
 
-stow -t $HOME -d machine/$(hostname) firefox
+# install applications from homebrew
+brew install --cask iterm2
+
+# link config from this repository
 stow -t $HOME -d os/macos skhd
 stow -t $HOME -d os/macos yabai
 stow -t $HOME -d os/macos git
-stow -t $HOME conda
 stow -t $HOME nvim
 stow -t $HOME tmux
 stow -t $HOME zsh
+
+# link config from nextcloud
+if [[ ! -d ~/nextcloud.shamith.net ]]; then
+    stow -t $HOME -d ~/nextcloud.shamith.net/computing secrets
+    stow -t $HOME -d ~/nextcloud.shamith.net/computing rclone
+else
+    exit "~/nextcloud.shamith.net directory required"
+fi
