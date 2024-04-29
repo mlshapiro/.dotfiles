bb5ff59ffa09611c208f26f82b3a47d231607fa7
Marc Shapiro <git@ms.tengr.net>
2024-04-29 17:00:13 -0400

    Add tabline

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -84,5 +84,21 @@ return {
                 },
             })
         end
-    }
+    },
+    {
+        'romgrk/barbar.nvim',
+        version = '^1.0.0',
+        dependencies = {
+            'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+            'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+        },
+        init = function() 
+            vim.g.barbar_auto_setup = false 
+        end,
+        opts = {
+            -- animation = true,
+            -- insert_at_start = true,
+            -- …etc.
+        },
+    },
 }
