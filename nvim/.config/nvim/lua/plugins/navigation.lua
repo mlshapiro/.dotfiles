01d29b80a4cd9afac9b28d3152f1f033a3ff3c82
Marc Shapiro <git@ms.tengr.net>
2024-05-22 09:39:10 -0400

    Fix formatting, fuzzy finder in hidden fiels

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -22,14 +22,20 @@ return {
 
             local actions = require("telescope.actions")
             require("telescope").setup({
-            defaults = {
-                mappings = {
-                  i = {
-                    ["<esc>"] = actions.close
-                  },
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<esc>"] = actions.close
+                        },
+                    },
                 },
-              }
-          })
+                pickers = {
+                    find_files = {
+                        -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+                        find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+                    }
+                }
+            })
         end
     },
 
@@ -145,7 +151,6 @@ return {
             -- vim.keymap.set('n', '<C-p>', '<Cmd>BufferPick<CR>')
 
             -- Sort automatically by...
-            vim.keymap.set('n', '<leader>bb', '<Cmd>BufferOrderByBufferNumber<CR>')
             vim.keymap.set('n', '<leader>bn', '<Cmd>BufferOrderByName<CR>')
             vim.keymap.set('n', '<leader>bd', '<Cmd>BufferOrderByDirectory<CR>')
             vim.keymap.set('n', '<leader>bl', '<Cmd>BufferOrderByLanguage<CR>')
