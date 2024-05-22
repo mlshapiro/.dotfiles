a3ba1d3ccbf26dfc17cf436b03bca3f5368c2f01
Marc Shapiro <git@ms.tengr.net>
2024-05-22 09:50:42 -0400

    Add fzf plugin

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -40,6 +40,10 @@ return {
     },
 
     {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    },
+    {
         "folke/trouble.nvim",
         cond = not vim.g.vscode,
         config = function()
